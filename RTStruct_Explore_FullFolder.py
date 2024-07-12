67466210d9a21e2ecf3fc847238b76fc75bc0bcf
Luis de la O <46694700+ldelaoa@users.noreply.github.com>
2024-07-12 15:46:36 +0200

    Update RTStruct_Explore_FullFolder.py

--- RTStruct_Explore_FullFolder.py
+++ RTStruct_Explore_FullFolder.py
@@ -2,6 +2,7 @@ import pydicom
 import os
 import csv
 from tqdm import tqdm
+from multiprocessing import Pool
 
 def getLabels(rtstruct_path,px):
     rtstruct_dicom = pydicom.dcmread(rtstruct_path,stop_before_pixels=True)
@@ -15,19 +16,24 @@ def getLabels(rtstruct_path,px):
         date = rtstruct_dicom.StudyDate
         return [px,date[:4],names_struct]
     return [px,None,None]
-
+    
 def main(root,csv_file_path):
+    p = Pool()  # number of processes = number of CPUs
+    tasks = []
+    pxList = os.listdir(root)
+    for currPx in tqdm(pxList):
+        for subroot,dirs,files in os.walk(os.path.join(root,currPx)):
+            for d in dirs:
+                if "rtstruct" in d.lower() or "pproved" in d:
+                    rtstructFile_list = os.listdir(os.path.join(subroot,d))
+                    for currRTfile in rtstructFile_list:
+                        tasks.append((os.path.join(subroot,d,currRTfile),currPx))
+    results = p.map(getLabels, tasks)
     with open (csv_file_path,'w',newline='') as file:
         writer = csv.writer(file)
-        pxList = os.listdir(root)
-        for currPx in tqdm(pxList):
-            for subroot,dirs,files in os.walk(os.path.join(root,currPx)):
-                for d in dirs:
-                    if "rtstruct" in d.lower() or "pproved" in d:
-                        rtstructFile_list = os.listdir(os.path.join(subroot,d))
-                        for currRTfile in rtstructFile_list:
-                            writer.writerow(getLabels(os.path.join(subroot,d,currRTfile),currPx))
-
+        for result in results:
+            writer.writerow(result)
+            
 if __name__ == '__main__':
     root = "//zkh/appdata/RTDicom/Projectline_modelling_lung_cancer/DICOM_data/DATA_VOLLEDIG_unstructured/"
     csv_file_path = "//zkh/appdata/RTDicom/Projectline_modelling_lung_cancer/Users/Luis/CSVs/rtstruct_Dicom_AllPX.csv"
